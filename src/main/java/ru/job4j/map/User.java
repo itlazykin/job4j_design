e5ed9ab8c41870ae6aee2c665ae67404e277f16a
itlazykin <dzaka89@gmail.com>
2024-09-03 16:32:55 +0300

    5. Переопределить только hashCode [#1003 #409979]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,15 +1,50 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
-    private String name;
-    private int children;
-    private Calendar birthday;
+    private final String name;
+    private final int children;
+    private final Calendar birthday;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        User den = new User("Den", 2, calendar);
+        int hashCodeDen = den.hashCode();
+        int hashDen = hashCodeDen ^ (hashCodeDen >>> 16);
+        int bucketDen = hashDen & 15;
+        User ben = new User("Den", 2, calendar);
+        int hashCodeBen = ben.hashCode();
+        int hashBen = hashCodeBen ^ (hashCodeBen >>> 16);
+        int bucketBen = hashBen & 15;
+        Map<User, Object> map = new HashMap<>(16);
+        map.put(den, new Object());
+        map.put(ben, new Object());
+        System.out.println(map);
+        System.out.println("Без переопределения equals и hashCode");
+        System.out.println("Пары попали в разные корзины");
+        System.out.println("Проверка ключей на равенство по hashcode и equals не происходила, т.к. ключи расположены в "
+                + "разных бакетах, и сравнение не производится");
+        System.out.println();
+        System.out.println("Переопределить только hashCode");
+        System.out.println("Пары попали в одну корзину");
+        System.out.println("Проверка по hashCode была выполнена, тк мы его переопределили и пары были в одной корзине");
+        System.out.println("Проверка по equals производилась, но т.к. этот метод не переопределен в классе юзер, то "
+                + "происходило сравнение ссылок на объект, а не значений объекта.");
+        System.out.println();
     }
 }
