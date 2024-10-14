df357e165e6de0a546db710a0f69f655263ae00e
itlazykin <dzaka89@gmail.com>
2024-10-14 14:56:50 +0300

    Обход дерева в ширину [#504972]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,29 +1,29 @@
 package ru.job4j.collection;
 
-import java.util.NoSuchElementException;
-
-public class SimpleQueue<T> {
-    private final SimpleStack<T> input = new SimpleStack<>();
-    private final SimpleStack<T> output = new SimpleStack<>();
-    private int size;
+public class SimpleQueue<T> implements Queue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+    private int countIn;
+    private int countOut;
 
     public T poll() {
-        if (size == 0) {
-            throw new NoSuchElementException("Queue is empty");
+        if (countOut == 0) {
+            while (countIn != 0) {
+                out.push(in.pop());
+                countIn--;
+                countOut++;
+            }
         }
-        for (int i = 0; i < size; i++) {
-            output.push(input.pop());
-        }
-        T result = output.pop();
-        size--;
-        for (int i = 0; i < size; i++) {
-            input.push(output.pop());
-        }
-        return result;
+        countOut--;
+        return out.pop();
     }
 
     public void push(T value) {
-        input.push(value);
-        size++;
+        in.push(value);
+        countIn++;
+    }
+
+    public int size() {
+        return countIn + countOut;
     }
 }
---
+++ src/main/java/ru/job4j/newcoll/tree/Node.java
@@ -0,0 +1,38 @@
+package ru.job4j.newcoll.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Модель узла.
+ *
+ * @param <E>
+ */
+public class Node<E> {
+    private final E value;
+    private final List<Node<E>> children = new ArrayList<>();
+
+    public Node(E value) {
+        this.value = value;
+    }
+
+    @SafeVarargs
+    public Node(E value, Node<E>... children) {
+        this.value = value;
+        this.children.addAll(Arrays.asList(children));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Node{ %s }", value);
+    }
+
+    public List<Node<E>> getChildren() {
+        return children;
+    }
+
+    public E getValue() {
+        return value;
+    }
+}
