aec614b5ba87f585de2b136247e2b0e1773c70b0
itlazykin <dzaka89@gmail.com>
2024-10-25 10:56:57 +0300

    8. Тестирование IO [#173905]. Написал javadoc на метод.

--- src/main/java/ru/job4j/io/Abuse.java
+++ src/main/java/ru/job4j/io/Abuse.java
@@ -5,6 +5,21 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class Abuse {
+    /**
+     * Удаляет из исходного файла указанные слова и сохраняет результат в целевой файл.
+     *
+     * <p>
+     * Метод читает строки из исходного файла, разбивает их на слова,
+     * отфильтровывает указанные в списке `words` слова и записывает результат
+     * в целевой файл. Каждое слово разделяется пробелом. Используется
+     * автоматическое закрытие ресурсов (try-with-resources).
+     * </p>
+     *
+     * @param source путь к исходному файлу, из которого читаются данные
+     * @param target путь к целевому файлу, в который будет записан результат
+     * @param words  список слов, которые необходимо удалить из текста
+     * @throws IOException если возникают ошибки ввода-вывода при работе с файлами
+     */
     public static void drop(String source, String target, List<String> words) throws IOException {
         try (BufferedReader input = new BufferedReader(new FileReader(source));
              PrintWriter output = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
