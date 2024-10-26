87c74cc4e61a0d865414cc6af8d94c32148d3871
itlazykin <dzaka89@gmail.com>
2024-10-26 20:40:51 +0300

    Видео уроки Трегулова. Введение в стримы. FileWriter.

---
+++ src/main/java/ru/job4j/io/tregulove/FileWriterEx.java
@@ -0,0 +1,28 @@
+package ru.job4j.io.tregulove;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
+public class FileWriterEx {
+    public static void main(String[] args) throws IOException {
+        String rubai = """
+                Можно соблазнить мужчину, у которого есть жена.
+                Можно соблазнить мужчину, у которого есть любовница.
+                Но нельзя соблазнить мужчину,
+                У которого есть любимая женщина.
+                """;
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("test1");
+            for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }
+            System.out.println("Done!");
+        } catch (IOException e) {
+            System.err.println("In/out error" + e.getMessage());
+        } finally {
+            Objects.requireNonNull(writer).close();
+        }
+    }
+}
