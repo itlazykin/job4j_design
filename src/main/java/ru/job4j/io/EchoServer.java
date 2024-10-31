25fb6bce679d3303a50b9aaaa30bcc55e2f07857
itlazykin <dzaka89@gmail.com>
2024-10-31 22:27:08 +0300

    2. Что такое Socket? [#4850 #519762]. Правки по задаче.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,18 +1,14 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -21,17 +17,10 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String in = input.readLine();
-                    if ("/?msg=Exit".contains(in)) {
-                        System.out.println(in);
+                    if (in.contains("/?msg=Bye")) {
+                        System.out.println("Good bye.See you next time.");
                         server.close();
                         break;
-                    }
-                    if (in.contains("?msg=Hello")) {
-                        output.write("Hello".getBytes());
-                        System.out.println(in);
-                    } else {
-                        output.write("What".getBytes());
-                        System.out.println(in);
                     }
                     for (String string = input.readLine();
                          string != null && !string.isEmpty();
@@ -39,12 +28,8 @@ public class EchoServer {
                         System.out.println(string);
                     }
                     output.flush();
-                } catch (Exception e) {
-                    LOG.error("Input/output error", e);
                 }
             }
-        } catch (Exception e) {
-            LOG.error("Server error", e);
         }
     }
 }
