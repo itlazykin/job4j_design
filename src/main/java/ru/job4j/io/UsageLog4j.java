2d77f007ea456e57cf153b35e07d6b4c5e554b0c
itlazykin <dzaka89@gmail.com>
2024-10-31 22:37:59 +0300

    3. Slf4j - вывод переменных. [#268852 #519783]. Правки по задаче.

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,17 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        try {
-            throw new Exception("Not supported code");
-        } catch (Exception e) {
-            LOG.error("Exception in log example", e);
-        }
+        String name = "Denis Lazykin";
+        byte age = 35;
+        short height = 183;
+        int weight = 70;
+        long neurones = 86_000_000_000L;
+        float hair = 100_000F;
+        double bacteria = 420.24_000_000_000_000D;
+        char gender = 'M';
+        boolean learnJava = true;
+        LOG.debug("User info name : {}, age : {}, height : {}, weight : {}, neurones : {},"
+                        + " hair : {}, bacteria : {}, gender : {}, learnJava : {}",
+                name, age, height, weight, neurones, hair, bacteria, gender, learnJava);
     }
 }
