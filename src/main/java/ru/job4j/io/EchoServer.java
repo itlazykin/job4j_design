f8b27c3acb789c98a0ed79b6f384dbe2e1fefefc
itlazykin <dzaka89@gmail.com>
2024-11-01 21:27:57 +0300

    2. Что такое Socket? [#4850 #519762]. Добавил пояснения к коду.

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,12 +12,32 @@ public class EchoServer {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServer.class.getName());
 
     public static void main(String[] args) {
+        /*
+        Создаем сервер. Вызов конструктора ServerSocket создает серверный сокет, привязанный к указанному порту.
+        Чтобы клиент мог узнать, где находится сервер ему нужен адрес и порт, 9000 - это порт.
+        По умолчанию адрес будет localhost.
+         */
         try (ServerSocket server = new ServerSocket(9000)) {
+            /*
+            Сервер работает, пока его принудительно не закроют.
+             */
             while (!server.isClosed()) {
+                /*
+                Вызов метода accept() заставляет программу ждать подключений по указанному порту, работа программы
+                продолжится только после подключения клиента. После успешного подключения
+                 метод возвращает объект Socket, который используется для взаимодействия с клиентом.
+                 */
                 Socket socket = server.accept();
+                /*
+                С помощью объекта Socket программа может получить входной поток
+                и может отправить данные в выходной поток.
+                 */
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    /*
+                    В ответ мы записываем строчку.
+                     */
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     String in = input.readLine();
                     if (in.contains("/?msg=Exit")) {
@@ -32,11 +52,17 @@ public class EchoServer {
                         output.write("What".getBytes());
                         System.out.println(in);
                     }
+                    /*
+                    В программе читается весь входной поток.
+                     */
                     for (String string = input.readLine();
                          string != null && !string.isEmpty();
                          string = input.readLine()) {
                         System.out.println(string);
                     }
+                    /*
+                    После чтения отправляем ответ окончательно.
+                     */
                     output.flush();
                 } catch (Exception e) {
                     LOG.error("Input/output error", e);
