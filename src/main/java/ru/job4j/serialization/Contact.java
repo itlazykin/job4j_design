dbadd03089492e5da39c9a77536edf0630489bd4
itlazykin <dzaka89@gmail.com>
2024-11-01 10:16:10 +0300

    1. Что такое Сериализация? [#313163]

---
+++ src/main/java/ru/job4j/serialization/Contact.java
@@ -0,0 +1,75 @@
+package ru.job4j.serialization;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Objects;
+
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 420L;
+    private final int zipCode;
+    private final String phone;
+
+    public Contact(int zipCode, String phone) {
+        this.zipCode = zipCode;
+        this.phone = phone;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{"
+                + "zipCode=" + zipCode
+                + ", phone='" + phone + '\''
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Contact contact = (Contact) object;
+        return zipCode == contact.zipCode && Objects.equals(phone, contact.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zipCode, phone);
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        final Contact contact = new Contact(420420, "+7 (420) 420 - 04 - 20");
+       /*
+       Запись объекта во временный файл, который удалится системой
+        */
+        File tempFile = Files.createTempFile(null, null).toFile();
+        try (FileOutputStream fos = new FileOutputStream(tempFile);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(contact);
+            System.out.println(contact + " Сериализация");
+        }
+        /*
+        Чтение объекта из файла
+         */
+        try (FileInputStream fis = new FileInputStream(tempFile);
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
+            final Contact contactFromFile = (Contact) ois.readObject();
+            System.out.println(contactFromFile + " Десериализация");
+            if (contactFromFile.equals(contact)) {
+                System.out.println("Десериализованный объект соответствует первоначальному.");
+            } else {
+                System.out.println("Десериализованный объект не соответствует первоначальному.");
+            }
+        }
+    }
+}
