6458641754eb45968a8264e4c34da3ad992eb26f
itlazykin <dzaka89@gmail.com>
2024-11-19 17:08:35 +0300

    4. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/User.java
@@ -0,0 +1,33 @@
+package ru.job4j.gc.leak;
+
+import java.util.Objects;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+}
