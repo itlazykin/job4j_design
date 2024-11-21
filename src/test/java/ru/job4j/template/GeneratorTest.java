191d42535e16e41c2df0d8b09ff1441006f3a4a8
itlazykin <dzaka89@gmail.com>
2024-11-21 12:41:52 +0300

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class GeneratorTest {
+    @Disabled
+    @Test
+    void whenAllKeysArePresentThenGenerateCorrectString() {
+        Generator generator = new UsageGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Denis");
+        map.put("subject", "you");
+        String result = generator.produce(template, map);
+        assertThat(result).isEqualTo("I am a Denis, Who are you?");
+    }
+
+    @Test
+    void whenMissingKeysThenThrowException() {
+        Generator generator = new UsageGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Denis");
+        assertThatThrownBy(() -> generator.produce(template, map))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Missing key: subject");
+    }
+
+    @Test
+    void whenExtraKeysThenThrowException() {
+        Generator generator = new UsageGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Denis");
+        map.put("subject", "you");
+        map.put("extra", "extraValue");
+        assertThatThrownBy(() -> generator.produce(template, map))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Unexpected key: extra");
+    }
+}
